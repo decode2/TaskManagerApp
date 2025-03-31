3312db3dc02700e00a70a46be3678b4a1524cf46
Matías D. <matiasmoreno52@gmail.com>
2025-03-31 04:38:36 -0300

    update darkmode background

--- client-ts/src/index.tsx
+++ client-ts/src/index.tsx
@@ -4,13 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-
-root.render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <div className="min-h-screen transition-colors duration-300 bg-inherit">
+      <App />
+    </div>
   </React.StrictMode>
 );
 
