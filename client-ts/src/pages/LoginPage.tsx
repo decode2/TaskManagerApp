44bdedf4c1e1945adf3423c7364174cf06d909ca
Matías D. <matiasmoreno52@gmail.com>
2025-03-31 04:38:36 -0300

    update darkmode background

--- client-ts/src/pages/LoginPage.tsx
+++ client-ts/src/pages/LoginPage.tsx
@@ -2,14 +2,13 @@ import LoginForm from "../components/LoginForm";
 import useDarkMode from "../hooks/useDarkMode";
 
 export default function LoginPage() {
-  
-  return (
-    <div
-      className="min-h-screen flex items-center justify-center transition-colors duration-300
-                bg-gradient-to-br from-blue-100 to-blue-300 
-                dark:from-slate-900 dark:to-slate-950"
-    >
+  useDarkMode();
 
+  return (
+    <div className="min-h-screen transition-colors duration-300 bg-gradient-to-br 
+                    from-blue-100 to-blue-300 
+                    dark:from-slate-900 dark:to-slate-950 
+                    flex items-center justify-center px-4">
       <LoginForm />
     </div>
   );
